7b2f5ac375009105cfb75979a05b2d5b990d1621
Adam Richter <adam_richter2004@yahoo.com>
2016-10-24 14:39:23 -0700

    Fix arguments to lxc-create.

--- portworx-kernel-tester/container_driver.lxc.sh
+++ portworx-kernel-tester/container_driver.lxc.sh
@@ -7,19 +7,24 @@ container_name=
 
 start_container_lxc() {
     local release
-    container_name="pwx_test_${distribution}"
+    container_name="pwx_test_${distro}"
 
     apt-get install -y lxc
 
-    case "$distribution" in
+    case "$distro" in
 	ubuntu ) release="xenial" ;;
 	debian ) release="jessie" ;;
 	centos ) release="Core" ;;
+	* )
+	    echo "start_container_lxc: Unknown distribution \"${distro}\"." >&2
+	    echo "Failing." >&2
+	    return 1 ;;
     esac
 
     if [ ! -e "/var/lib/lxc/${container_name}" ] ; then
 	lxc-create --name "${container_name}" --template download -- \
-		   --dist "$distribution" --arch "$arch" --release "$release"
+		     --dist "$distro" --arch "$arch" --release "$release"
+
 	in_container_lxc "$@"
     fi
 
