14e62d8dfb38719444987dddaaf9e85b85977bcb
Adam Richter <adam_richter2004@yahoo.com>
2016-10-25 04:23:46 -0700

    Make install_pkgs_dir_deb use "sh -c" in calling in_container to get file wildcard expansion to work.  Also, for now, install git in the container, as pxfuse seems to want it.  I am not sure why pxfuse wants it.

--- portworx-kernel-tester/distro_driver.deb.sh
+++ portworx-kernel-tester/distro_driver.deb.sh
@@ -2,10 +2,12 @@
 # script.
 
 dist_init_container_deb() {
+    echo "AJR distro_driver.deb.sh dist_init_container_deb $* called." >&2
     in_container apt-get update
     # ^^^ Skip this for binary reproducibility ??
 
-    in_container apt-get install -y autoconf gcc g++ make tar libssl1.0
+    in_container apt-get install -y autoconf g++ gcc git libssl1.0 make tar
+    # Why px-fuse wants git is unclear to me.
 }
 
 pkg_files_to_kernel_dirs_deb() {
@@ -28,7 +30,7 @@ pkg_files_to_names_deb () {
     done
 }
 
-install_pkgs_deb()      { in_container apt-get install -y "$@" ; }
-install_pkgs_dir_deb()  { in_container dpkg --install --force-all "$1/*" ; }
+install_pkgs_deb()      { in_container apt-get install --quiet --yes "$@" ; }
+install_pkgs_dir_deb()  { in_container sh -c "dpkg --install --force-all $1/*" ; }
 uninstall_pkgs_deb()    { in_container dpkg --remove "$@" ; }
-pkgs_update_deb()       { in_container apt-get update -y ; }
+pkgs_update_deb()       { in_container apt-get update --quiet --yes ; }
