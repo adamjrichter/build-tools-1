9a262a1b0cb8f61f1edfef49b26d5c00b099a24e
Adam Richter <adam_richter2004@yahoo.com>
2016-10-25 04:18:23 -0700

    Add await_default_route and await_dns to deal with race in starting a Linux container versus running apt-get commands.  Clear unitialized containers before checking if the container we want already exists.

--- portworx-kernel-tester/container_driver.lxc.sh
+++ portworx-kernel-tester/container_driver.lxc.sh
@@ -1,4 +1,4 @@
-# To be sourced by a shell script
+# To be sourced by a bash script (bash because this file uses "[[")
 
 # Global variables:
 container_name=
@@ -7,8 +7,32 @@ container_name=
 
 use_lxc_attach=true
 
+await_default_route() {
+    local count=100
+    while [[ $count -gt 0 ]] ; do
+	if in_container_lxc ip route | egrep --quiet '^default via ' ; then
+	    return 0
+	fi
+	sleep 0.1
+	count=$((count - 1))
+    done
+    return 1
+}
+
+await_dns() {
+    local count=100
+    while [[ $count -gt 0 ]] ; do
+	if in_container_lxc test -e /etc/resolv.conf ; then
+	    return 0
+	fi
+	sleep 0.1
+	count=$((count - 1))
+    done
+    return 1
+}
+
 start_container_lxc() {
-    local release
+    local release must_initialize
     container_name="pwx_test_${distro}"
 
     apt-get install -y lxc
@@ -23,15 +47,28 @@ start_container_lxc() {
 	    return 1 ;;
     esac
 
-    if [ ! -e "/var/lib/lxc/${container_name}" ] ; then
+    lxc-ls > /dev/null 2>&1 || true
+    # ^^^ Removes incompletely initialized containters
+
+    if [[ -e "/var/lib/lxc/${container_name}" ]] ; then
+	must_initialize=false
+    else
+	must_initialize=true
 	lxc-create --name "${container_name}" --template download -- \
 		     --dist "$distro" --arch "$arch" --release "$release"
-
-	"$@"
     fi
 
     if $use_lxc_attach ; then
         lxc-start --name "${container_name}" --daemon
+	await_default_route
+	if ! await_dns ; then
+	    in_container_lxc tee /etc/resolve.conf \
+			     < /etc/resolv.conf > /dev/null
+	fi
+    fi
+
+    if $must_initialize ; then
+	"$@"
     fi
 }
 
