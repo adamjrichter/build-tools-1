d6901de31017234a5dbb9c3d9522d48b1df37472
Adam Richter <adam_richter2004@yahoo.com>
2016-10-26 19:06:53 -0700

    Move all of the main commands into a main function name mirror_subdirs.

--- portworx-mirror-server/mirror-kernels.ubuntu.sh
+++ portworx-mirror-server/mirror-kernels.ubuntu.sh
@@ -8,9 +8,7 @@ cd ${mirrordir} || exit $?
 mirrordir=/home/ftp/mirrors
 
 
-url_dir=kernel.ubuntu.com/~kernel-ppa/mainline
-top_url=http://$url_dir
-top_dir=http/$url_dir
+top_url=http://kernel.ubuntu.com/~kernel-ppa/mainline
 
 #arch=i386
 arch=amd64
@@ -56,21 +54,36 @@ get_subdir_index_files() {
 	      --accept=index.html --recursive
 }
 
-rm -f ${top_dir}/index.html
-wget --force-directories --protocol-directories ${top_url}/
+url_to_dir()
+{
+    local url="$1"
+    local prefix="${url%%://*}"
+    local suffix="${url#:*//}"
+    echo "$prefix/$suffix"
+}
 
-# FIXME.  This breaks for subdirectory names containing spaces.
-subdirs=$(extract_subdirs <  ${top_dir}/index.html | versions_above_3_9)
+mirror_subdirs() {
+    local top_url="$1"
+    local top_dir
+    top_dir=$(url_to_dir "$top_url")
+    rm -f ${top_dir}/index.html
+    wget --force-directories --protocol-directories ${top_url}/
 
-get_subdir_index_files "$top_url"
+    # FIXME.  This breaks for subdirectory names containing spaces.
+    subdirs=$(extract_subdirs <  ${top_dir}/index.html | versions_above_3_9)
 
-# TODO: Change "image" to "headers"
-for subdir in $subdirs ; do
-    #which=image
-    which=headers
-    subdir_no_slash=${subdir%/}
-    extract_subdirs < "${top_dir}/$subdir/index.html" |
-	egrep "linux-${which}-[0-9].*_(${arch}|all).deb" |
-	subdirs_to_urls "${top_url}/${subdir_no_slash}"
-done |
-    xargs -- wget ${TIMESTAMPING} --protocol-directories --force-directories
+    get_subdir_index_files "$top_url"
+
+    # TODO: Change "image" to "headers"
+    for subdir in $subdirs ; do
+	#which=image
+	which=headers
+	subdir_no_slash=${subdir%/}
+	extract_subdirs < "${top_dir}/$subdir/index.html" |
+	    egrep "linux-${which}-[0-9].*_(${arch}|all).deb" |
+	    subdirs_to_urls "${top_url}/${subdir_no_slash}"
+    done |
+	xargs -- wget ${TIMESTAMPING} --protocol-directories --force-directories
+}
+
+mirror_subdirs "$top_url"
