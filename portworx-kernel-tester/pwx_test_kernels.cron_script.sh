b9706c08b9f04da401f2bfbf044ddcac799c8f2d
Adam Richter <adam_richter2004@yahoo.com>
2016-10-28 07:17:53 -0700

    Enable Debian testing (in addition to Centos and Ubuntu) in nightly cron job.

--- portworx-kernel-tester/pwx_test_kernels.cron_script.sh
+++ portworx-kernel-tester/pwx_test_kernels.cron_script.sh
@@ -4,7 +4,6 @@ logdir=/home/ubuntu/pwx_test_kernels.cron_script.sh.log
 
 exec > "$logdir" 2>&1
 
-#for distribution in centos debian ubuntu ; do
-for dist in centos ubuntu ; do
+for distribution in centos debian ubuntu ; do
     pwx_test_kernels_in_mirror.sh --containers=lxc --distribution="$dist"
 done
