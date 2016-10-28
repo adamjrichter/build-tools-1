3a88515f0e60059595c78b078ed01b23bcb65548
Adam Richter <adam_richter2004@yahoo.com>
2016-10-27 18:23:10 -0700

    Run Centos kernel tests nightly.  They do not quite work yet, but they only take a couple of minutes because there are only a few kernels to try.

--- portworx-kernel-tester/pwx_test_kernels.cron_script.sh
+++ portworx-kernel-tester/pwx_test_kernels.cron_script.sh
@@ -5,6 +5,6 @@ logdir=/home/ubuntu/pwx_test_kernels.cron_script.sh.log
 exec > "$logdir" 2>&1
 
 #for distribution in centos debian ubuntu ; do
-for dist in ubuntu ; do
+for dist in centos ubuntu ; do
     pwx_test_kernels_in_mirror.sh --containers=lxc --distribution="$dist"
 done
