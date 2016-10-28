e16dfada5a0f3eea11bb463ae0c27e4ec13b63c5
Adam Richter <adam_richter2004@yahoo.com>
2016-10-27 17:45:32 -0700

    Add "--assumeyes --quiet" to yum commands.

--- portworx-kernel-tester/distro_driver.rpm.sh
+++ portworx-kernel-tester/distro_driver.rpm.sh
@@ -19,8 +19,8 @@ pkg_files_to_names_rpm () {
     rpm --query --package "$@"
 }
 
-install_pkgs_rpm() { in_container yum install "$@" ; }
+install_pkgs_rpm() { in_container yum --assumeyes --quiet install "$@" ; }
 install_pkg_dir_rpm() { in_container sh -c "rpm --install $1/*" ; }
 uninstall_pkgs_rpm() { in_container rpm --erase "$@" ; }
 
-pkgs_update_rpm() { in_container "yum update" ; }
+pkgs_update_rpm() { in_container yum --assumeyes --quiet update ; }
