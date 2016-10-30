ddb12f8d62304b5b0df563741ae2612eea2482f1
Adam Richter <adam_richter2004@yahoo.com>
2016-10-30 01:30:56 -0700

    Return the last non-zero status if any kernel test script returned non-zero.  Otherwise, return zero (success).

--- portworx-kernel-tester/pwx_test_kernels.cron_script.sh
+++ portworx-kernel-tester/pwx_test_kernels.cron_script.sh
@@ -4,6 +4,11 @@ logdir=/home/ubuntu/pwx_test_kernels.cron_script.sh.log
 
 exec > "$logdir" 2>&1
 
+result=0
 for distribution in centos debian ubuntu ; do
-    pwx_test_kernels_in_mirror.sh --containers=lxc --distribution="$dist"
+    if ! pwx_test_kernels_in_mirror.sh --containers=lxc --distribution="$dist" ; then
+	result=$?
+    fi
 done
+
+exit $result
