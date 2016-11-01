98ca83710e16f8e21811729202b6bfd02a66a1d2
Adam Richter <adam_richter2004@yahoo.com>
2016-11-01 03:09:09 -0700

    Oops.  I forgot to add pwx-mirror-util.sh a few commits earlier.

---
+++ portworx-mirror-server/pwx-mirror-util.sh
@@ -0,0 +1,36 @@
+#
+# This file is intended to be sourced from a shell script.
+
+url_to_dir()
+{
+    local url="$1"
+    local prefix="${url%%://*}"
+    local suffix="${url#*://}"
+    echo "$prefix/$suffix"
+}
+
+echo_word_per_line() {
+    local word
+    for word in "$@" ; do
+	echo "$word"
+    done
+}
+
+newlines_around_angle_brackets() {
+    sed 's/</\'$'\n''</g;s/>/>\'$'\n''/g;'
+}
+
+# I think there is a perl program named extract-urls that will do this better.
+extract_subdirs() {
+    newlines_around_angle_brackets |
+	egrep '^<a href="' |
+	sed 's/^[^"]*"//;s/"[^"]*$//'
+}
+
+subdirs_to_urls() {
+    local top_url="$1"
+
+    while read subdir ; do
+        echo "$top_url/$subdir"
+    done
+}
