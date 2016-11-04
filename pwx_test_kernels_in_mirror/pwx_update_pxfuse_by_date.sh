7d5b9c2c18386121ec9f29af912f23e648095d91
Adam Richter <adam_richter2004@yahoo.com>
2016-11-04 07:15:44 -0700

    Fix determination of symlink contents in ~ftp/build-results/pxfuse/by-date.

--- pwx_test_kernels_in_mirror/pwx_update_pxfuse_by_date.sh
+++ pwx_test_kernels_in_mirror/pwx_update_pxfuse_by_date.sh
@@ -7,11 +7,14 @@ cd "$datedir" || exit $?
 
 for dir in ../by-checksum/*/ ; do
     dir_no_slash=${dir%/}
-    prefix=$( (TZ='' stat --format=%y "$dir_no_slash") | sed 's/ +0000$//;s/ /_/g' )
-    suffix=${dir_no_slash#../}
-    ln -s "$dir" "${prefix}-${suffix}"
+
+    prefix=$( (TZ='' stat --format=%y "$dir_no_slash") |
+	      sed 's/ +0000$//;s/ /_/g' )
+
+    suffix=${dir_no_slash#../by-checksum/}
+    ln -s "$dir_no_slash" "${prefix}-${suffix}"
 done
 
 latest=$(ls -1dtr ../by-checksum/*/ | tail -1)
 rm -f latest
-ln -s "$latest" latest
+ln -s "${latest%/}" latest
