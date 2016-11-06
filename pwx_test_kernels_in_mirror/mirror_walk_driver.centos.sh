67b25a10e962eb811ea4f24916a043846393b46a
Adam Richter <adam_richter2004@yahoo.com>
2016-11-06 09:34:02 -0800

    Fix conversion of names like directory/kernel-headers-version.arch.rpm to /directory/kernel-devel-version.arch.rpm.  Also, handle names like kernel-ml-headers being converted to kernel-ml-devel (I think "ml" stands for "mainline" in this case).

--- pwx_test_kernels_in_mirror/mirror_walk_driver.centos.sh
+++ pwx_test_kernels_in_mirror/mirror_walk_driver.centos.sh
@@ -14,7 +14,8 @@ get_default_mirror_dirs_centos()
 
 walk_mirror_centos() {
     local mirror_tree="$1"
-    local file rpm_arch return_status
+    local file dir base devel_base base_without_kernel middle devel_file
+    local rpm_arch return_status
 
     shift 1
 
@@ -26,10 +27,30 @@ walk_mirror_centos() {
 
     return_status=0
     find "$mirror_tree" -name "kernel-*headers-*.${rpm_arch}.rpm" -type f -print0 |
-    while read -r -d $'\0' file ; do
-        if ! "$@" "$file" ; then
-	    return_status=$?
-	fi
-    done
+	while read -r -d $'\0' file ; do
+	    dir=${file%/*}
+	    base=${file##*/}
+	    base_without_kernel="${base#kernel-}"
+	    middle="${base_without_kernel%headers*}"
+	    devel_base="kernel-${middle}devel-${base#kernel*-headers-}"
+	    devel_file="$dir/$devel_base"
+	    echo "AJR mirror_walk_driver.centos.sh: walk_mirror_centos:" >&2
+	    echo "    dir=$dir" >&2
+	    echo "    base=$base" >&2
+	    echo "    devel_base=$devel_base" >&2
+	    echo "    devel_file=$devel_file" >&2
+	    if ! [[ -e "$devel_file" ]] ; then
+		echo "    devel_file not found" >&2
+		devel_file=""
+	    else
+		echo "    devel_file found" >&2
+	    fi
+	    echo "" >&2
+	    # This assumes devel_file does not have spaces in its name:
+            if ! "$@" "$file" $devel_file ; then
+		return_status=$?
+	    fi
+	done
+
     return $return_status
 }
