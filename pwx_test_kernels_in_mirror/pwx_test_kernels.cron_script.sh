8a9061b79a533f7242d389eaba58b9ed9a0cf55a
Adam Richter <adam_richter2004@yahoo.com>
2016-11-08 00:51:29 -0800

    Use newly added "--leave-containers-running" optimization option for pwx_test_kernel_pkgs.sh.

--- pwx_test_kernels_in_mirror/pwx_test_kernels.cron_script.sh
+++ pwx_test_kernels_in_mirror/pwx_test_kernels.cron_script.sh
@@ -9,13 +9,26 @@ exec > "$log_file" 2>&1 < /dev/null
 PATH=/usr/local/bin:$PATH
 export PATH
 
+stop_lxc_test_containers() {
+    local dist="$1"
+
+    lxc-ls -1 |
+	egrep "^pwx_test_${dist}_" |
+	while read container ; do
+	    lxc-stop --name "$container"
+	done
+}
+
 result=0
 for dist in centos debian fedora ubuntu ; do
-    if ! pwx_test_kernels_in_mirror.sh --containers=lxc --distribution="$dist" ; then
+    if ! pwx_test_kernels_in_mirror.sh --containers=lxc --distribution="$dist" --command="pwx_test_kernels_in_mirror.sh --leave-containers-running" ; then
 	result=$?
     fi
+    stop_lxc_test_containers "$dist"
 done
 
 $scriptsdir/pwx_update_pxfuse_by_date.sh
+
+echo "pwx_test_kernels.cron_script.sh: result=$result"
 
 exit $result
