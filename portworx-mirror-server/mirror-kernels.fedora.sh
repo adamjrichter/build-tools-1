e712110779065b1f894442a84bd844429977de4f
Adam Richter <adam_richter2004@yahoo.com>
2016-11-15 12:14:57 -0800

    Do not mirror kernel-*headers*.rpm.  Only mirror kernel-*devel*.rpm.

--- portworx-mirror-server/mirror-kernels.fedora.sh
+++ portworx-mirror-server/mirror-kernels.fedora.sh
@@ -31,17 +31,17 @@ mirror_ncsu_edu() {
 	sed "s|^|${top_url}|;s|\$|/Everything/x86_64/os/Packages/k/|" |
 	xargs wget ${QUIET} --no-parent ${TIMESTAMPING} -e robots=off \
 	 --protocol-directories --force-directories --recursive --level=1 \
-	 --accept-regex="/(index.html)|(kernel-(headers|devel).*\.rpm)"
+	 --accept-regex="/(index.html)|(kernel-devel.*\.rpm)"
 
 	 # This version would add kernel-xxx-heades, but currently only
 	 # matches kernel-cross-headers on ftp.linux.ncsu.edu:
-	 # --accept-regex="/(index.html)|(kernel-.*(headers|devel).*\.rpm)"
+	 # --accept-regex="/(index.html)|(kernel-.*devel.*\.rpm)"
 	 
     # FIXME.  The following regular expresion might filter out kernels before
     # 3.10.  It is modified from one that was not working, but maybe this
     # version might work.
     #
-    # --accept-regex="/(index.html)|(kernel-(.*-)?(headers|devel)-${above_3_9_regexp}(.*-.*-.*)?\..*\.rpm)"
+    # --accept-regex="/(index.html)|(kernel-(.*-)?devel-${above_3_9_regexp}(.*-.*-.*)?\..*\.rpm)"
     save_error
 }
 
