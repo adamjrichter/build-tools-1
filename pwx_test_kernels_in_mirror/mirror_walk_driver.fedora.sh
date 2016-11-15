709ed8dae1d6cab462b6ae1a123168f45c92cf89
Adam Richter <adam_richter2004@yahoo.com>
2016-11-15 09:44:58 -0800

    Only test kernel versions 3.10 or later, and, if the kernel-devel packages is names kernel-<some_word>-devel, make sure that <some-work> consists only of alphabetic characters and no hyphens, to avoid to unrelated package with names like "tools-libs" in them.

--- pwx_test_kernels_in_mirror/mirror_walk_driver.fedora.sh
+++ pwx_test_kernels_in_mirror/mirror_walk_driver.fedora.sh
@@ -23,7 +23,9 @@ walk_mirror_fedora() {
     fi
 
     return_status=0
-    find "$mirror_tree" -name "kernel-devel-*.${rpm_arch}.rpm" -type f -print0 |
+
+    kernel_regexp=".*/kernel-([a-z]+-)?devel-${above_3_9_regexp}[0-9.]*-.*${rpm_arch}.rpm"
+    find "$mirror_tree" -regextype egrep -regex "$kernel_regexp" -type f -print0 |
 	while read -r -d $'\0' file ; do
             if ! "$@" "$file" ; then
 		return_status=$?
