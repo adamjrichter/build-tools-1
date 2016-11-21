55ff15298346fc4e2b5b63bbc3c7376e28ba1e19
Adam Richter <adam_richter2004@yahoo.com>
2016-11-21 08:31:46 -0800

    Might as well also confirm that pxfuse_dir is a directory.

--- pwx_test_kernel_pkgs/pwx_test_kernel_pkgs_one_container.sh
+++ pwx_test_kernel_pkgs/pwx_test_kernel_pkgs_one_container.sh
@@ -81,6 +81,11 @@ if ! [[ -e "$pxfuse_dir" ]] ; then
     exit 1
 fi
 
+if ! [[ -d "$pxfuse_dir" ]] ; then
+    echo "$0: pxfuse directory ${pxfuse_dir} is not a directory." >&2
+    exit 1
+fi
+
 local_tmp_dir=/tmp/test-kernels.ubuntu.$$
 
 test_kernel_pkgs() {
