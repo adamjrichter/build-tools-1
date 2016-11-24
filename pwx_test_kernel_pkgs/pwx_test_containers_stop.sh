3316c37f5d997a9dd24e5cc2def8f8a0b913e96e
Adam Richter <adam_richter2004@yahoo.com>
2016-11-24 00:31:44 -0800

    Not currently used by the scripts, may be a convenient system administration tool, so install it for now.

---
+++ pwx_test_kernel_pkgs/pwx_test_containers_stop.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+# ^^^^^^^^^ bash because it uses "[["
+# Stops all Linux containers that pwx_test_containers might have started.
+
+scriptsdir=$PWD
+
+usage() {
+    echo "Usage: pwx_test_containers_stop.sh [--distribution=dist] [--containers=container_system] [--releases=releases]"
+    echo "    Stops all containers used by pwx_test_kernel_pkgs for the"
+    echo "    specified operating system distribution releases."
+    echo ""
+}
+
+arch=amd64
+distro=ubuntu
+distro_releases=""
+container_system=lxc
+
+
+. ${scriptsdir}/container_driver.sh
+. ${scriptsdir}/distro_driver.sh
+
+while [[ $# -gt 0 ]] ; do
+    case "$1" in
+	--arch=* ) arch=${1#--arch=} ;;	  # Unused, but allow it for now.
+	--containers=* ) container_system=${1#--containers=} ;;
+	--distribution=* ) distro=${1#--distribution=} ;;
+	--releases=* ) distro_release=${1#--releases=} ;;
+	--* ) usage >&2 ; exit 1 ;;
+	* ) break ;;
+    esac
+    shift
+done
+
+main()
+{
+    local releaes releases
+
+    if [[ -n "$distro_releases" ]] ; then
+	releases=$(echo "$distro_releases" | sed 's/,/ /g')
+    else
+	releases=$(get_dist_releases)
+    fi
+
+    for release in $releases ; do
+	stop_container --release="$release"
+    done
+}
+
+main
