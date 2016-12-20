3c44805839546afeca2cd45b1ee9dd74cf7933d2
Adam Richter <adam_richter2004@yahoo.com>
2016-12-20 11:49:47 -0800

    Add mirror http://vault.centos.org/centos, in exactly the same format as http://mirror.centos.org/centos.

--- pwx_test_kernels_in_mirror/mirror_walk_driver.centos.sh
+++ pwx_test_kernels_in_mirror/mirror_walk_driver.centos.sh
@@ -10,6 +10,7 @@ get_default_mirror_dirs_centos()
     echo \
 	/home/ftp/mirrors/http/elrepo.org/linux/kernel \
 	/home/ftp/mirrors/http/mirror.centos.org/centos
+	/home/ftp/mirrors/http/vault.centos.org/centos
 }
 
 walk_mirror_centos() {
