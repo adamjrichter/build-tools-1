3e30a77051a6ab81aa0bc96af9b9b19a1dc0789b
Adam Richter <adam_richter2004@yahoo.com>
2016-12-22 01:08:57 -0800

    Add mirror for http://mirrors.coreix.net/elrepo-archive-archive/kernel/.

--- pwx_test_kernels_in_mirror/mirror_walk_driver.centos.sh
+++ pwx_test_kernels_in_mirror/mirror_walk_driver.centos.sh
@@ -10,7 +10,8 @@ get_default_mirror_dirs_centos()
     echo \
 	/home/ftp/mirrors/http/elrepo.org/linux/kernel \
 	/home/ftp/mirrors/http/mirror.centos.org/centos \
-	/home/ftp/mirrors/http/vault.centos.org/centos
+	/home/ftp/mirrors/http/vault.centos.org/centos \
+	/home/ftp/mirrors/mirrors.coreix.net/elrepo-archive-archive/kernel
 
     # echo /home/ftp/mirrors/http/dev.centos.org
 }
