795647fdd32bcce77fcc91d2c5ed0ef1b9c13985
Adam Richter <adam_richter2004@yahoo.com>
2017-06-29 14:04:47 -0700

    Turn off GPG signature checking for now, to support DVD directories more easily.  Perhaps later, this can be replaced by using repository data from the DVD directories.

--- pwx_test_kernels_in_mirror/mirror_walk_driver.opensuse.sh
+++ pwx_test_kernels_in_mirror/mirror_walk_driver.opensuse.sh
@@ -9,7 +9,8 @@ get_default_mirror_dirs_opensuse()
 {
     echo \
 	/home/ftp/mirrors/http/download.opensuse.org/distribution \
-	/home/ftp/mirrors/http/download.opensuse.org/update
+	/home/ftp/mirrors/http/download.opensuse.org/update \
+	/home/ftp/mirrors/dvd/suse
 
     # echo /home/ftp/mirrors/http/dev.opensuse.org
 }
