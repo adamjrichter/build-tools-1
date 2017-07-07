6248aadae52658642fe07205b8c8fb21c782c9f3
Adam Richter <adam_richter2004@yahoo.com>
2017-07-07 16:48:57 -0700

    Add portworx-mirror-server/mirror-kernels.coreos.sh.

---
+++ portworx-mirror-server/mirror-kernels.coreos.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+# ^^ requires bash because save_error() calls bash_stack_trace,
+# which uses bash-specific command "caller".
+
+scriptsdir=$PWD
+. ${scriptsdir}/pwx-mirror-config.sh
+. ${scriptsdir}/pwx-mirror-util.sh
+mkdir -p ${mirrordir}
+cd ${mirrordir} || exit $?
+
+mirrordir=/home/ftp/mirrors
+
+
+#arch=i386
+arch=amd64
+
+TIMESTAMPING=--timestamping
+#TIMESTAMPING=--no-clobber
+
+mirror_coreos() {
+    local release="$1"
+    local prefix="https://${release}.release.core-os.net"
+    
+    wget ${TIMESTAMPING} --quiet --protocol-directories \
+	 --force-directories --accept=index.html --recursive \
+	 "${prefix}/"
+
+    wget ${TIMESTAMPING} --quiet --protocol-directories \
+	 --force-directories --accept=index.html \
+	 "${prefix}/${arch}-usr/current/coreos_production_iso_image.iso"
+}
+
+mirror_coreos stable
+# mirror_coreos alpha
+# mirror_coreos beta
