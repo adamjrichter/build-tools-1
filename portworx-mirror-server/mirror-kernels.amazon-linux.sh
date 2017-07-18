ea207abb3cf9887dc31984ac12def87709b73445
Adam Richter <adam_richter2004@yahoo.com>
2017-07-17 17:52:24 -0700

    Numerous changes to work around what appear to me to be reposync bugs.  Most importantly, remove /var/tmp/yum-$(whoami)-* before querying a different repository.

--- portworx-mirror-server/mirror-kernels.amazon-linux.sh
+++ portworx-mirror-server/mirror-kernels.amazon-linux.sh
@@ -10,6 +10,7 @@ scriptsdir=$PWD
 . ${scriptsdir}/pwx-mirror-util.sh
 cd ${mirrordir} || exit $?
 mkdir -p ${mirrordir}
+amzn_confdir_template=/home/pwxmirror/mirror-scripts/from_amazon_linux
 
 # TIMESTAMPING=--timestamping
 TIMESTAMPING='--no-clobber --no-use-server-timestamps'
@@ -18,7 +19,7 @@ QUIET=
 
 error_code=0
 
-list_amazon_linux_versions() {
+amzn_list_versions() {
     local endyear=$(date +%Y)
     local year
 
@@ -33,33 +34,48 @@ list_amazon_linux_versions() {
     done
 }
 
-list_amazon_linux_urls() {
+amzn_init_confdir() {
+    local confdir="$1"
+
+    rm -rf "$confdir" "/var/tmp/yum-$(whoami)-"*
+    cp -apr "${amzn_confdir_template}" "$confdir"
+    mkdir -p "${confdir}/var/log" "${confdir}/var/cache/yum"
+    sed "s|=/|=${confdir}/|" \
+            < ${amzn_confdir_template}/etc/yum.conf |
+    ( egrep -v '^(reposdir|installroot|releasever)='
+      echo "installroot=${confdir}/"  # Prefix directory of etc/yum/vars.
+      echo "reposdir=${confdir}/etc/yum.repos.d/" ) \
+	> "${confdir}/etc/yum.conf"
+}
+
+amzn_list_urls() {
     # Amazon releases are four_digit_year.two_digit_month, where four_digit
     # year starts at 2011, and two_digit_month is 03 or 09.
     local confdir=/tmp/amazon-config.$$
     local release_ver repos
-    
-    rm -rf "$confdir"
-    cp -apr /home/pwxmirror/mirror-scripts/from_amazon_linux "$confdir"
-    ( egrep -v '^(reposdir|installroot|releasever)=' \
-	    < /home/pwxmirror/mirror-scripts/from_amazon_linux/etc/yum.conf
-      echo "installroot=${confdir}/"  # Prefix directory of etc/yum/vars.
-      echo "reposdir=${confdir}/etc/yum.repos.d/" ) > "${confdir}/etc/yum.conf"
 
-    repos=$(cat "$confdir/etc/yum.repos.d"/*.repo |
+    repos=$(cat "${amzn_confdir_template}/etc/yum.repos.d"/*.repo |
 		   egrep '^\['  |
-		   sed 's/^\[/ --repo=/;s/\]$//')
-    list_amazon_linux_versions |
-	while read release_ver ; do
-	    # FIXME.  This seems to look for the latest release anyhow.
-	    echo "$release_ver" > "$confdir/etc/yum/vars/releasever"
-	    echo "releasever=${release_ver}" >> "$confdir/etc/yum.conf"
-	    rm -rf "${confdir}/download"
-	    mkdir -p "${confdir}/download"
+		   sed 's/^\[/ --repo=/;s/\]$//' )
+
+    amzn_list_versions | while read release_ver ; do
+
+        amzn_init_confdir "${confdir}"
+
+	# FIXME.  This seems to look for the latest release anyhow.
+	echo -n "$release_ver" > "$confdir/etc/yum/vars/releasever"
+	echo "releasever=${release_ver}" >> "$confdir/etc/yum.conf"
+	rm -rf "${confdir}/download" "${confdir}/var/lib/yum"
+	mkdir -p "${confdir}/download"
+	# Apparently "--source" only works when repos are specified.  Also,
+	# reposync will abort if an unknown repo is specified.  So, run
+	# reposync separately on echo repo.
+	for repo in $repos ; do
 	    reposync --download_path="${confdir}/download" --urls --source \
-		     --config="$confdir/etc/yum.conf" $repos --quiet \
-		     2> /dev/null
-	done |
+		     --config="${confdir}/etc/yum.conf" $repo
+	done
+#	--quiet 2> /dev/null
+    done |
 	egrep 'kernel-devel.*\.rpm|/SRPMS/kernel-.*.src.rpm'
 
     # The following filter should eliminate paths to the same filename:
@@ -70,10 +86,10 @@ list_amazon_linux_urls() {
 }
 
 mirror_amazon_linux() {
-    list_amazon_linux_urls |
+    amzn_list_urls |
 	sort -u |
 	wget --protocol-directories --force-directories --no-clobber \
-	     --quiet --input-file=-
+	     --input-file=-
 
     save_error
 }
