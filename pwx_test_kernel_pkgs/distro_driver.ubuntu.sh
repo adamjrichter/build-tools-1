c0846d1d75e593d77c90a166e12176db6d0df516
Adam Richter <adam_richter2004@yahoo.com>
2017-07-29 01:20:54 -0700

    Install bc in Ubuntu containers, which is not necessary for the Ubuntu builds, but is used by a script for the ChromiumOS configuration, which currently uses Ubuntu for building.

--- pwx_test_kernel_pkgs/distro_driver.ubuntu.sh
+++ pwx_test_kernel_pkgs/distro_driver.ubuntu.sh
@@ -3,7 +3,6 @@
 
 # For now, just default everything to the common Debian drivers.
 
-dist_init_container_ubuntu()      { dist_init_container_deb       "$@" ; }
 pkg_files_to_kernel_dirs_ubuntu() { pkg_files_to_kernel_dirs_deb  "$@" ; }
 pkg_files_to_dependencies_ubuntu() { pkg_files_to_dependencies_deb "$@" ; }
 pkg_files_to_names_ubuntu()       { pkg_files_to_names_deb        "$@" ; }
@@ -14,6 +13,12 @@ pkgs_update_ubuntu()              { pkgs_update_deb               "$@" ; }
 dist_clean_up_container_ubuntu()  { dist_clean_up_container_deb   "$@" ; }
 dist_start_container_ubuntu()     { dist_start_container_deb      "$@"; }
 start_container_ubuntu()          { start_container_generic "$@" ; }
+
+dist_init_container_ubuntu() {
+    dist_init_container_deb       "$@"
+    install_pkgs bc	# Not needed for Ubuntu, but ChromiumOS wants it.
+}
+
 
 get_dist_releases_ubuntu()
 {
