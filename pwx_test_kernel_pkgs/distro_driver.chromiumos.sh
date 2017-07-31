14e2e7d173a6b94d7a48f2b834f8ff6346a53001
Adam Richter <adam_richter2004@yahoo.com>
2017-07-31 16:50:10 -0700

    Use the new ${distro}_build_func() interface that replaces and extends ${distro}_prepare_build(), as it is now possible to override how px.ko is built.  Also, use a cached kernel build if available, and save the kernel build if building it is successful.

--- pwx_test_kernel_pkgs/distro_driver.chromiumos.sh
+++ pwx_test_kernel_pkgs/distro_driver.chromiumos.sh
@@ -56,22 +56,65 @@ dist_clean_up_container_chromiumos()
     dist_clean_up_container_ubuntu   "$@"
 }
 
-chromiumos_prepare_build() {
+chromiumos_build() {
     local container_tmpdir="$1"
-    local branch="$3"
+    local headers_dir="$2"
+    local make_args="$3"
+    local log_subdir="$4"
+    local branch="$5"
+    local commit_name=${log_subdir##*/}
+    local commit_tgz="/home/ftp/cache/kernels/chromiumos/${commit_name}.tar.gz"
+    local build_dir
+
+    # Currently, px.ko does not depend on any other kernel modules, so
+    # we could probably skip making the kernel modules.  If you want to try
+    # this optimization, set the variable make_modules_command to "true" (a
+    # no-op) instead of "make modules":
+    #
+    # local make_modules_command="true"
+    local make_modules_command="make modules"
 
     install_pkgs curl
     # FIXME?  Is it necessory to "apt-get install" some other packages,
     # besides curl?
 
-    in_container sh -c \
+    if [[ -e "${commit_tgz}" ]] ; then
+	in_container sh -c \
+            "rm -rf ${container_tmpdir}/kernel &&
+             cd ${container_tmpdir} &&
+	     tar xpz kernel" < "${commit_tgz}" || return $?
+	headers_dir="${container_tmpdir}/kernel"
+    else
+	in_container sh -c \
                "cd ${chromiumos_remote_tmp_dir} &&
 		git clean --force &&
 		git checkout ${branch} &&
 		./chromeos/scripts/prepareconfig chromiumos-x86_64 &&
 		make prepare &&
-		make scripts"
-    # 		make &&
-    #		make modules
-    # "make && make modules" is for kernel symbol versioning.
+		make scripts" || return $?
+    fi
+
+    default_build_func "${container_tmpdir}" "${headers_dir}" "${make_args}" ||
+	return $?
+
+    if [[ -e "${commit_tgz}" ]] ; then
+	return 0
+    fi
+
+    # Build the whole kernel (and possibly modules, depending on the
+    # value of ${make_modules_command}), just to have kernel symbol
+    # versioning information against which px.ko.
+    in_container sh -c \
+        "cd ${container_tmpdir}/kernel &&
+	 make vmlinux &&
+	 ${make_modules_command} &&
+         cd ${container_tmpdir}/pxfuse_dir &&
+	 make clean" || return $?
+
+    default_build_func "${container_tmpdir}" "${headers_dir}" "${make_args}" ||
+	return $?
+
+    mkdir -p "${commit_tgz%/*}"
+    in_container sh -c "cd ${container_tmpdir} && tar cz kernel" \
+		 > "${commit_tgz}"
 }
